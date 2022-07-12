2da2559ff309607d04235d0d7f1e306b0c310006
kjy <kjy@kjyui-MacBookPro.local>
2022-07-12 15:44:45 +0900

    [kjy] Start ToDoProject

---
+++ todo/src/test/java/com/worknjoy/todo/TodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.worknjoy.todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
